6d20119db50ecae4a2e5a6d524179287aa99d48b
glacier16127 <109465716+glacier16127@users.noreply.github.com>
2022-11-13 17:27:19 +0800

    isok

---
+++ dibiao/static/js/myjavascript.js
@@ -0,0 +1,158 @@
+// 弹出图打开关闭模块
+function open1() {
+    document.getElementById('db01').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close1() {
+    document.getElementById('db01').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open2() {
+    document.getElementById('db02').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close2() {
+    document.getElementById('db02').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open3() {
+    document.getElementById('db03').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close3() {
+    document.getElementById('db03').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open4() {
+    document.getElementById('db04').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close4() {
+    document.getElementById('db04').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+
+function open5() {
+    document.getElementById('db05').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close5() {
+    document.getElementById('db05').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+
+function open6() {
+    document.getElementById('db06').style.display = 'block';
+    document.getElementById('fade').style.display = 'block'
+}
+function close6() {
+    document.getElementById('db06').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open7() {
+    document.getElementById('db07').style.display = 'block';
+    document.getElementById('fade').style.display = 'block'
+}
+function close7() {
+    document.getElementById('db07').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open8() {
+    document.getElementById('db08').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close8() {
+    document.getElementById('db08').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open9() {
+    document.getElementById('db09').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close9() {
+    document.getElementById('db09').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open10() {
+    document.getElementById('db0_10').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close10() {
+    document.getElementById('db0_10').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open11() {
+    document.getElementById('db0_11').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close11() {
+    document.getElementById('db0_11').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open12() {
+    document.getElementById('db0_12').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close12() {
+    document.getElementById('db0_12').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open13() {
+    document.getElementById('db0_13').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close13() {
+    document.getElementById('db0_13').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open14() {
+    document.getElementById('db0_14').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close14() {
+    document.getElementById('db0_14').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open15() {
+    document.getElementById('db0_15').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close15() {
+    document.getElementById('db0_15').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open16() {
+    document.getElementById('db0_16').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close16() {
+    document.getElementById('db0_16').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open17() {
+    document.getElementById('db0_17').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close17() {
+    document.getElementById('db0_17').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+function open18() {
+    document.getElementById('db0_18').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+}
+function close18() {
+    document.getElementById('db0_18').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+}
+// 弹出图打开关闭模块end
+// 按钮类名shangla1,xiala1
+window.onload=function() {
+    var shangla_1=document.getElementById("shangla1");
+    var xiala_1 =document.getElementById("xiala1");
+};
+console.log(shangla_1);
+console.log(xiala_1);
+shangla_1.addEventListener('click',function() {
+    alert(1);
+});

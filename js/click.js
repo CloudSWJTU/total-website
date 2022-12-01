62a31a5797c879d0b98adfc4ce8e033570fbd545
glacier16127 <109465716+glacier16127@users.noreply.github.com>
2022-12-01 19:22:56 +0800

    2022.12.1-18 index

---
+++ js/click.js
@@ -0,0 +1,8 @@
+function deleteminipart(){
+   document.getElementById("minipart").style.display="none";
+   document.getElementById("tackbackbutton").style.display="block";
+}
+function takebackminipart(){
+    document.getElementById("minipart").style.display="block";
+    document.getElementById("tackbackbutton").style.display="none";
+}

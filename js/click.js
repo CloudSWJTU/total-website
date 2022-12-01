344ce495eb27fa99f0fbdc26f08542b2fc5892ae
glacier16127 <109465716+glacier16127@users.noreply.github.com>
2022-12-01 23:47:51 +0800

    还差中间三个

--- js/click.js
+++ js/click.js
@@ -6,3 +6,9 @@ function takebackminipart(){
     document.getElementById("minipart").style.display="block";
     document.getElementById("tackbackbutton").style.display="none";
 }
+function minipartchangecolor1(){
+    document.getElementById("minipart").style.backgroundColor="#2196F3";
+}
+function minipartchangecolor2(){
+    document.getElementById("minipart").style.backgroundColor="rgba(0, 137, 249, 0.5)";
+}
